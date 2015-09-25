651426fd8d237d17f8922719ff6f3dc0c70d1675
Javis Wu <wujavis@gmail.com>
2015-09-25 11:41:52 -0400

    broken notes

---
+++ Sept25/dic.py
@@ -0,0 +1,23 @@
+d={}
+d['one']=1
+d['two']='two'
+d['three']='hello'
+ #d.haskey('two') = true
+ #d.haskey('twppppp') = false
+ #d.keys() lists
+for x in dL
+   print x, d[x]
+
+d2={'item1':1234,
+    'item2':d,
+    'hello':'world'}
+
+
+l=[1,2,3,'hello']
+elist=[]
+# l[-1] wraps around other side
+#l[1:3] elemts at 1,2 exclude 3
+#
+
+
+    
